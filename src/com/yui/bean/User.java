dda565596f654df67b0559b940f4090a3805235a
dotyui <1132662352@qq.com>
2021-08-11 01:33:36 +0800

    v01

---
+++ src/com/yui/bean/User.java
@@ -0,0 +1,34 @@
+package com.yui.bean;
+
+/**
+ * @Author: Administrator
+ * @Date: 2021/8/11
+ * @Description:
+ * @Modified By:
+ */
+public class User {
+    String username;
+    String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
---
+++ src/com/yui/bean/test.java
@@ -0,0 +1,13 @@
+package com.yui.bean;
+
+/**
+ * @Author: Administrator
+ * @Date: 2021/8/11
+ * @Description:
+ * @Modified By:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
